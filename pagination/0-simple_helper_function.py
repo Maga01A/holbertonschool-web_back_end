593f529814a226140cda855b48abe25621750a02
Maga01A <alnatluxanov@gmail.com>
2026-01-18 09:35:40 +0000

    task pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,10 +6,9 @@ Pagination helper function
 
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Səhifə nömrəsi və səhifə ölçüsünə əsasən
-    başlanğıc və bitiş indekslərini ehtiva edən tuple qaytarır.
+    Return a tuple of size two containing a start index and an end index.
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    
+
     return (start_index, end_index)
