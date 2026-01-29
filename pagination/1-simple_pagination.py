829ca222874b5eaaddd54763c4390c52ba61e2f2
Maga01A <alnatluxanov@gmail.com>
2026-01-29 10:29:57 +0000

    Pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Səhifə nömrəsi və ölçüsünə əsasən başlanğıc və son indeksləri qaytarır.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Datasetdən müvafiq səhifəni qaytarır.
+        """
+        # Arqumentlərin tam ədəd və 0-dan böyük olmasını yoxlayırıq
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        # İndeksləri tapırıq
+        start, end = index_range(page, page_size)
+        
+        # Dataset-i yükləyirik
+        data = self.dataset()
+        
+        # Əgər indekslər dataset hüdudlarından kənardırsa, boş siyahı qaytarırıq
+        if start >= len(data):
+            return []
+            
+        return data[start:end]
